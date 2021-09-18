6ec254dd2b11f7f92f8381948f1006ede9028d75
norkitorki <norki@tuta.io>
2021-09-18 10:59:08 +0200

    Make moveset method private and include Trackable module

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,11 @@
 # frozen-string-literal: true
 
+require_relative 'trackable'
+
 # chess knight class
 class Knight
+  include Trackable
+
   attr_reader :position, :moves
   attr_accessor :symbol
 
@@ -15,13 +19,6 @@ class Knight
     @moves    = moveset
   end
 
-  def moveset(position = self.position)
-    return [] unless valid_position?(position)
-
-    vectors = MOVES.map { |x, y| [position.first + x, position.last + y] }
-    vectors.select { |vec| valid_position?(vec) }
-  end
-
   def knight_moves(destination, queue = [position])
     current = queue.shift
     return [destination] if current == destination
@@ -32,29 +29,13 @@ class Knight
     knight_moves(destination, queue + children)
   end
 
-  def track_path(board, path)
-    return if path.length < 2
-
-    vectors = []
-    (0..path.length - 2).each do |i|
-      vectors += track_positions([path[i], path[i + 1]])
-    end
-    vectors.each { |vec| board.place(vec, "\e[0;32;49m●\e[0m") }
-    path
-  end
-
   private
 
-  def track_positions(move)
-    (1..2).map do |i|
-      if (move[0].first - move[1].first).abs == 2
-        min = move.min_by(&:first)
-        [min.first + i, min.last]
-      else
-        max = move.max_by(&:last)
-        [max.first, max.last - i]
-      end
-    end
+  def moveset(position = self.position)
+    return [] unless valid_position?(position)
+
+    vectors = MOVES.map { |x, y| [position.first + x, position.last + y] }
+    vectors.select { |vec| valid_position?(vec) }
   end
 
   def valid_position?(vector)
